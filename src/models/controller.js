706b64ad2a9ff35547b0a15990a3614035b1e844
jesus1a <48657218+jesus1a@users.noreply.github.com>
2022-11-13 05:06:40 -0800

    Create controller.js
    only function right now just adds a student given the paramaters of name, address, useName, password

---
+++ src/models/controller.js
@@ -0,0 +1,19 @@
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+    host: "fcfs.c2oe7fkglsr2.us-west-2.rds.amazonaws.com",
+    user: "admin",
+    password: "529dh-bj345-wbedaj",
+    database: "FCFS"
+});
+function studentReg(gname, gaddress, guserName, gpassword){
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("connected!");
+    var sql = "INSERT INTO Students (name, address, userName, password) VALUES (${gname}, ${gaddress}, ${guserName}, ${gpassword})";
+    con.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log("1 record inserted");
+    });
+});
+}
